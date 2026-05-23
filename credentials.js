900f87105bfa845fb7deb782974cf743dd256f36
MacknCheesy <kenziezenker@kw.com>
2026-05-23 02:31:29 +0000

    Deploying to gh-pages from @ MacknCheesy/Planner@376cc3d40418ef2f347770aac23d68547711a2d5 🚀

--- credentials.js
+++ credentials.js
@@ -1,2 +1,2 @@
-const API_KEY = 'AIzaSyDJTOkaRSoOFbD73cWA7jQmsdPnll89QgY';
+const API_KEY = 'AIzaSyAYte8a37EvS79vk0edjYPKw1b6sHUS3EE';
 const CLIENT_ID = '863170089189-9mqk50uut741dut23e00th4vaa8rd6ca.apps.googleusercontent.com';
